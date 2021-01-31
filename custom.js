b3d36461df7ecd6ba9f8a90de0daf6763dcf792b
Khalid Saifullah <khalid0132@gmail.com>
2021-01-31 01:51:31 +0100

    3rd

--- custom.js
+++ custom.js
@@ -1,10 +1,12 @@
 // GeneratePin section
 const generatePin = document.getElementById("generatePin");
 generatePin.addEventListener("click", function () {
-  const pinInput = (document.getElementById("pinInput").value = (
-    Math.random() * 10000 +
-    ""
-  ).split(".")[0]);
+  const random = Math.random() * 10000;
+  const pinInput = (random + "").split(".")[0];
+  //   const pinInput = (document.getElementById("pinInput").value = (
+  //     Math.random() * 10000 +
+  //     ""
+  //   ).split(".")[0]);
   const createPin = parseInt(pinInput);
   document.getElementById("pinInput").value = createPin;
   //   document.getElementById("pinInput").value = "";
@@ -24,17 +26,20 @@ function clickButton(buttonValue) {
 // cancel button
 const cancelAll = document.getElementById("cancelAll");
 cancelAll.addEventListener("click", function () {
+  let input = document.getElementById("inputNumber");
   input.value = " ";
+
+  // aro kichu update korte hobe aikhane cancel a... < button tao update korte hobe
 });
 
 // submitButton
 let submitButton = document.getElementById("submitButton");
 submitButton.addEventListener("click", function () {
   const inputValue = document.getElementById("inputNumber").value;
-  pressedValue = parseInt(inputValue);
+  const generatedPin = document.getElementById("pinInput").value;
   //   const pinCode = pinInput.value;
 
-  if (document.getElementById("pinInput").value == pressedValue) {
+  if (inputValue === generatedPin) {
     alert(document.getElementById("okNotify").innerText);
   } else {
     alert(document.getElementById("notOkNotify").innerText);
